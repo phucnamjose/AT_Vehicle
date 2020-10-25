e3d1f5135b4e05dd031fdecb40003cedd453e281
Phuc Nam <phucnamjose@gmail.com>
2020-10-25 10:02:23 +0700

    Add project

---
+++ Code_ST/Vehicle/Core/Inc/main.h
@@ -0,0 +1,96 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define LED_D4_Pin GPIO_PIN_2
+#define LED_D4_GPIO_Port GPIOE
+#define LED_D5_Pin GPIO_PIN_3
+#define LED_D5_GPIO_Port GPIOE
+#define LED_USB_Pin GPIO_PIN_4
+#define LED_USB_GPIO_Port GPIOE
+#define USB_DETECT_Pin GPIO_PIN_5
+#define USB_DETECT_GPIO_Port GPIOE
+#define USB_PULL_Pin GPIO_PIN_6
+#define USB_PULL_GPIO_Port GPIOE
+#define STEP1_EN_Pin GPIO_PIN_7
+#define STEP1_EN_GPIO_Port GPIOE
+#define STEP1_DIR_Pin GPIO_PIN_8
+#define STEP1_DIR_GPIO_Port GPIOE
+#define STEP2_EN_Pin GPIO_PIN_9
+#define STEP2_EN_GPIO_Port GPIOE
+#define STEP2_DIR_Pin GPIO_PIN_10
+#define STEP2_DIR_GPIO_Port GPIOE
+#define LS2_Pin GPIO_PIN_13
+#define LS2_GPIO_Port GPIOB
+#define LS1_L_Pin GPIO_PIN_14
+#define LS1_L_GPIO_Port GPIOB
+#define LS1_R_Pin GPIO_PIN_15
+#define LS1_R_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
